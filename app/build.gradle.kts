58ba4686194851f388411b429cf757c34b7a04eb
Bigo Dev <dev@bigo.local>
2026-06-22 14:15:41 +0800

    Fix minSdk: Menaikkan minSdk ke 24 untuk kompatibilitas FFmpeg-Kit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.web"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
